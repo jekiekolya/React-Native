9f25e90da802f5c95341cb030506f900a9e18fbc
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-02-13 13:37:24 +0200

    add posts screen

--- screens/mainScreens/PostsScreen/PostsScreen.Styled.jsx
+++ screens/mainScreens/PostsScreen/PostsScreen.Styled.jsx
@@ -7,6 +7,75 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#FFFFFF",
   },
+  containerHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+
+    paddingHorizontal: 16,
+    paddingVertical: 32,
+    backgroundColor: "#FFFFFF",
+  },
+  userPhoto: {
+    width: 60,
+    height: 60,
+
+    marginRight: 8,
+
+    borderRadius: 16,
+  },
+  userName: {
+    fontWeight: "700",
+    fontSize: 13,
+    lineHeight: 15,
+
+    color: "#212121",
+  },
+  userEmail: {
+    fontWeight: "400",
+    fontSize: 11,
+    lineHeight: 13,
+
+    color: "#212121",
+  },
+  listItem: {
+    width: "100%",
+    paddingHorizontal: 16,
+
+    backgroundColor: "#FFFFFF",
+  },
+  imagePost: {
+    width: "100%",
+    height: 240,
+
+    marginBottom: 8,
+
+    borderRadius: 8,
+  },
+  titlePost: {
+    width: "100%",
+
+    marginBottom: 11,
+
+    fontWeight: "500",
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "left",
+  },
+  dataPost: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+    width: "100%",
+
+    paddingBottom: 32,
+  },
+  textPost: {
+    fontSize: 16,
+    lineHeight: 19,
+
+    color: "#212121",
+  },
 });
 
 export default styles;
--- screens/mainScreens/PostsScreen/PostsScreen.jsx
+++ screens/mainScreens/PostsScreen/PostsScreen.jsx
@@ -1,12 +1,81 @@
-import { Text, View } from "react-native";
+import { FlatList, Image, SafeAreaView, Text, View } from "react-native";
+
+// Data
+import posts from "../../../api/posts";
+
+// Components
+import CommentsEmptyIcon from "../../../assets/images/CommentsEmptyIcon";
+import CommentsIcon from "../../../assets/images/CommentsIcon";
+import LikeIcon from "../../../assets/images/LikeIcon";
+import LocationIcon from "../../../assets/images/LocationIcon";
 
 // Styles
 import styles from "./PostsScreen.Styled";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={posts}
+        ListHeaderComponent={
+          <View style={styles.containerHeader}>
+            <Image
+              source={{ uri: "https://i.stack.imgur.com/5Powi.jpg?s=192&g=1" }}
+              style={styles.userPhoto}
+            />
+            <View>
+              <Text style={styles.userName}>Natali Romanova</Text>
+              <Text style={styles.userEmail}>email@example.com</Text>
+            </View>
+          </View>
+        }
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Image
+              source={{
+                uri: item.imageUrl,
+              }}
+              style={styles.imagePost}
+            />
+            <Text style={styles.titlePost}>{item.title}</Text>
+            <View style={styles.dataPost}>
+              <View style={{ flexDirection: "row" }}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: 24,
+                  }}
+                >
+                  {item.countComments === 0 ? (
+                    <CommentsEmptyIcon style={{ marginRight: 6 }} />
+                  ) : (
+                    <CommentsIcon style={{ marginRight: 6 }} />
+                  )}
+
+                  <Text style={styles.textPost}>{item.countComments}</Text>
+                </View>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <LikeIcon style={{ marginRight: 6 }} />
+                  <Text style={styles.textPost}>{item.countLikes}</Text>
+                </View>
+              </View>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <LocationIcon style={{ marginRight: 4 }} />
+                <Text
+                  style={{
+                    ...styles.textPost,
+                    textDecorationLine: "underline",
+                  }}
+                >
+                  {item.location}
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
   );
 }
